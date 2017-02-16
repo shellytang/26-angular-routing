dda64b83c7f174ffa391141e6e1bc05c60bfea30
Geoffrey Cox <gcox3d@gmail.com>
2017-02-16 14:14:10 -0800

    added title property

--- lab-geoff/app/view/home/home-controller.js
+++ lab-geoff/app/view/home/home-controller.js
@@ -1,7 +1,9 @@
-require('./home-template.html');
+require('./home-style.scss');
 
 module.exports = ['$log', HomeController];
 
 function HomeController($log) {
   $log.debug('HomeController');
+
+  this.title = 'home';
 }
