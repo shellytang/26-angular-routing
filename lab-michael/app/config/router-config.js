2bfef82a4e11be5764888f18b84471a8f6e404c5
Michael Bishop <polishthebishop@gmail.com>
2017-02-15 20:02:24 -0800

    Home Route Working

---
+++ lab-michael/app/config/router-config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = ['$stateProvider', '$urlRouterProvider', routerConfig];
+
+function routerConfig($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.when('/', '/home');
+
+  let routes = [
+    {
+      name: 'home',
+      url: '/home',
+      template: require('../view/home/home.html'),
+      controller: 'HomeController',
+      controllerAs: 'homeCtrl'
+    }
+  ];
+
+  routes.forEach( route => {
+    $stateProvider.state(route);
+  });
+};
