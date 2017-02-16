4940032d2bbe45d5c0a186ad156f9de999146984
Geoffrey Cox <gcox3d@gmail.com>
2017-02-15 21:05:46 -0800

    fixed name issue with template

--- lab-geoff/app/config/route-config.js
+++ lab-geoff/app/config/route-config.js
@@ -2,17 +2,22 @@
 module.exports = ['$stateProvider', '$urlRouterProvider', routerConfig];
 
 function routerConfig($stateProvider, $urlRouterProvider) {
-  //urlRouterProvider when / -> /home
+
+  $urlRouterProvider.when('/', '/home');
 
   let routes = [
     {
       name: 'home',
       route: '/home',
-      template: require('../view/home/home-template.scss'),
+      template: require('../view/home/home-template.html'),
       controller: 'HomeController',
       controllerAs: 'homeCtrl'
     }
     // gallery
     // signup
   ];
+
+  routes.forEach( route => {
+    $stateProvider.state(route);
+  });
 }
