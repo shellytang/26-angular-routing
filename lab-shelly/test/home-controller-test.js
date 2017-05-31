ea716ce60a6f74ac60cc944b41d1b8b0b06cd6c1
Shelly Tang <shellytang@gmail.com>
2017-05-31 11:53:28 -0700

    fixed typo

--- lab-shelly/test/home-controller-test.js
+++ lab-shelly/test/home-controller-test.js
@@ -8,13 +8,13 @@ describe('Testing the home controller', function () {
   beforeEach(() => {
     angular.mock.module('routesApp');
     angular.mock.inject($controller => {
-      this.homeCtrl = new $controller('GalleryController');
+      this.homeCtrl = new $controller('HomeController');
       this.homeCtrl.$onInit();
     });
   });
 
   it('should have a title', () => {
-    expect(this.homeCtrl.title).toBe('The Gallery');
+    expect(this.homeCtrl.title).toBe('Welcome Home');
   });
 
 });
