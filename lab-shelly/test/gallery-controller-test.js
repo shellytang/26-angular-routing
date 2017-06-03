cce3288718ac4f58c7fc399d7e9b0941be346ccf
Shelly Tang <shellytang@gmail.com>
2017-06-03 16:38:34 -0700

    added back in afterEach

--- lab-shelly/test/gallery-controller-test.js
+++ lab-shelly/test/gallery-controller-test.js
@@ -7,15 +7,17 @@ describe('Testing the gallery controller ', function () {
 
   beforeEach(() => {
     angular.mock.module('routesApp');
-    angular.mock.inject($controller => {
+    angular.mock.inject(($rootScope, $controller) => {
+      this.$rootScope = $rootScope;
       this.galleryCtrl = new $controller('GalleryController');
       this.galleryCtrl.$onInit();
     });
   });
 
+  afterEach(() => this.$rootScope.$apply());
+
   it('should have a title', () => {
     expect(this.galleryCtrl.title).toBe('The Gallery');
   });
-
 
 });
--- lab-shelly/test/home-controller-test.js
+++ lab-shelly/test/home-controller-test.js
@@ -7,11 +7,14 @@ describe('Testing the home controller', function () {
 
   beforeEach(() => {
     angular.mock.module('routesApp');
-    angular.mock.inject($controller => {
+    angular.mock.inject(($rootScope, $controller) => {
+      this.$rootScope = $rootScope;
       this.homeCtrl = new $controller('HomeController');
       this.homeCtrl.$onInit();
     });
   });
+
+  afterEach(() => this.$rootScope.$apply());
 
   it('should have a title', () => {
     expect(this.homeCtrl.title).toBe('Welcome Home');
